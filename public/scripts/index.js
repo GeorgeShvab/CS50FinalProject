6c5ae9feca69cb6e5798e21256bdabc0033a60f3
Georgiy <georgiy.shvab@gmail.com>
2022-07-11 12:36:35 +0300

    Зробив адаптив

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,6 +1,13 @@
 window.addEventListener('DOMContentLoaded', () => {
     const exitBtn = document.getElementsByClassName('account-leave')[0]
 
+    const menuBtn = document.getElementsByClassName('menu-btn')[0]
+    const nav = document.getElementsByClassName('nav')[0]
+
+    menuBtn.addEventListener('click', () => {
+        nav.classList.toggle('active')
+    })
+
     if (exitBtn) {
         exitBtn.addEventListener('click', (e) => {
             document.cookie = 'Authorization='
